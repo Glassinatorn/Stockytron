cc3cdf70bb4c164ef7618698bc6099c210f066f2
Glassinatorn <gusten1995@gmail.com>
2020-06-20 14:27:45 +0200

    Added setup file to setup the needed enviroment

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /bin/python3
+
+import subprocess
+import sys
+
+print("You need to have pip installed.")
+dependencies = "pandas numpy sklearn keras matplotlib"
+device = ""
+while device not in ["CPU", "GPU", "cpu", "gpu"]:
+    device = input("Compute with CPU/GPU?\n")
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", dependencies])
+
