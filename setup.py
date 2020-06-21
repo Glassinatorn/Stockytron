f932a07d66d8f0a72797a7ae822f5948b47656e2
Glassinatorn <gusten1995@gmail.com>
2020-06-21 08:02:02 +0200

    Updated setup script

--- setup.py
+++ setup.py
@@ -4,10 +4,17 @@ import subprocess
 import sys
 
 print("You need to have pip installed.")
-dependencies = "pandas numpy sklearn keras matplotlib"
+dependencies = ["pandas", "numpy", "sklearn", "keras", "matplotlib"]
 device = ""
 while device not in ["CPU", "GPU", "cpu", "gpu"]:
     device = input("Compute with CPU/GPU?\n")
 
-subprocess.check_call([sys.executable, "-m", "pip", "install", dependencies])
+# choosing if to use cpu or gpu
+if device in ["cpu", "CPU"]:
+    dependencies.append("tensorflow")
+else:
+    dependencies.append("tensorflow-gpu")
 
+# installing packages
+for tmp in dependencies:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", tmp])
