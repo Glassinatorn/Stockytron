8b0a14fb97bafcdd61f3c3250d35badee42ff67e
Glassinatorn <gusten1995@gmail.com>
2020-06-29 00:40:08 +0200

    Removed uneeded check for gpu or cpu

--- setup.py
+++ setup.py
@@ -2,19 +2,17 @@
 
 import subprocess
 import sys
+import pymongo
 
 print("You need to have pip installed.")
-dependencies = ["pandas", "numpy", "sklearn", "keras", "matplotlib"]
-device = ""
-while device not in ["CPU", "GPU", "cpu", "gpu"]:
-    device = input("Compute with CPU/GPU?\n")
-
-# choosing if to use cpu or gpu
-if device in ["cpu", "CPU"]:
-    dependencies.append("tensorflow")
-else:
-    dependencies.append("tensorflow-gpu")
+dependencies = ["pandas", "numpy", "sklearn", "keras", "matplotlib",
+        "tensorflow"]
 
 # installing packages
 for tmp in dependencies:
     subprocess.check_call([sys.executable, "-m", "pip", "install", tmp])
+
+
+# setting up database
+db_client = pymongo.MongoClient("mongodb://localhost:27017/")
+stock_db = db_client["stocks"] if "stocks" in db_client.list_database_names()
