f7582161c7329d9be21a8e61c3eada559122cf51
Glassinatorn <gusten1995@gmail.com>
2020-08-09 22:34:59 +0200

    Changed config file for bot and store functions

---
+++ bot/store_data.py
@@ -0,0 +1,12 @@
+from bot.gather_data import get_file
+
+
+def stock_to_db(json_data):
+    """
+    Function to store stock data in available db
+
+    Parameters
+    ----------
+    json_data : dictionary with data to be stored.
+    """
+    config = get_file('.config')
