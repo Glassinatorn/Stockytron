eb20d946b9f965543f27e7ecabb618955a3cf19d
Glassinatorn <gusten1995@gmail.com>
2020-08-24 02:10:34 +0200

    Hopefully finalized setup of databases
    Moved upload script into script to create databases and made it more
    generalized to be easier to configure, hopefully. Also updated compose
    file to complement it. Finally removed the old upload script.

--- db/pg-init-scripts/create-multiple-postgresql-databases.sh
+++ db/pg-init-scripts/create-multiple-postgresql-databases.sh
@@ -4,19 +4,44 @@ set -e
 set -u
 
 function create_user_and_database() {
-    local database=$1
-    echo "  Creating user and database '$database'"
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE USER $database;
-	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+    local DATABASE=$1
+    echo "  Creating user and database '$DATABASE'"
+    psql -v EXIT_ON_ERROR=1 --username "$POSTGRES_USER" <<-EOSQL
+	    CREATE USER $DATABASE;
+	    CREATE DATABASE $DATABASE;
+	    GRANT ALL PRIVILEGES ON DATABASE $DATABASE TO $DATABASE;
 EOSQL
 }
 
-if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
-    echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
-    for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-        create_user_and_database $db
+if [ -n "$POSTGRES_DATABASES" ]; then
+    echo "Multiple database creation requested: $POSTGRES_DATABASES"
+    for DB in $(echo $POSTGRES_DATABASES | tr ',' ' '); do
+        create_user_and_database $DB
+
+        # creating and importing tables with data
+        if [ $DB != $DB_DJANGO ]; then
+            for FILE in /home/$DB/*.csv; do
+                NAME=$(printf "$FILE" |
+                    cut -d '/' -f 4 |
+                    cut -d '.' -f 1 |
+                    tr '[:upper:]' '[:lower:]')
+
+                psql -c "CREATE TABLE $NAME \
+                    (date date PRIMARY KEY, \
+                    open FLOAT, \
+                    high FLOAT, \
+                    low FLOAT, \
+                    close FLOAT, \
+                    adj FLOAT, \
+                    volume FLOAT);" \
+                    -U $POSTGRES_USER -d $DB
+
+                psql -c "\copy \
+                    $NAME
+                    FROM $FILE csv" -U $POSTGRES_USER -d $DB
+            done
+        fi
     done
+
     echo "Multiple databases created"
 fi
